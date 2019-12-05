f3358837318fa99fe4f974f69fc9bfa1a74a8741
kayleahJ218 <kayleah.wheless@seattlecolleges.edu>
2019-12-05 09:44:44 -0800

    1 file modified, 10 added, 3 deleted

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,9 +5,14 @@ import { FormsModule } from '@angular/forms';
 import { AppComponent } from './app.component';
 import { HelloComponent } from './hello.component';
 
+import { HomepageComponent } from './homepage.component';
+import { SocialmediaComponent } from './socialmedia.component';
+import { ResumeComponent } from './resume.component';
+
+import { AppRoutingModule } from './app.component';
 @NgModule({
   imports:      [ BrowserModule, FormsModule ],
-  declarations: [ AppComponent, HelloComponent ],
+  declarations: [ AppComponent, HelloComponent, HomepageComponent, SocialmediaComponent, ResumeComponent ],
   bootstrap:    [ AppComponent ]
 })
 export class AppModule { }
